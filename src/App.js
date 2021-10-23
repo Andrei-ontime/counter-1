bfff3fc23c8d72a6932c83b54a43542fd1fdc7c9
Andrei <58655937+Andrei-ontime@users.noreply.github.com>
2021-10-23 21:11:53 +0300

    Creating structure of a project with create-react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='wrapper'>
+        <div className='wrapper-counter'>1</div>
+        <div className='wrapper-btn'>
+          <button className='btn btn-add'>add</button>
+          <button className='btn btn-remove'>remove</button>
+        </div>
+      </div>
     </div>
   );
 }
